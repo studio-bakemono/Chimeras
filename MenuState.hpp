0cc8999a51c7ab2a8ae4ce533a4221852c6ce723
sweetsbeats <the.patton@yahoo.ca>
2018-11-14 12:17:33 -0800

    Added text to menu options, fixed memory leak with menuItems, naming issues

--- MenuState.hpp
+++ MenuState.hpp
@@ -6,18 +6,26 @@
 
 #include "State.hpp"
 #include <SFML/Graphics.hpp>
+
 #include <vector>
 #include <iostream>
+#include <cstdint>
+
+
 // Forward declaration, gets included in MenuState.cpp to avoid circular inclusion
 class Game;
+
 struct MenuItem;
-const int MenuLength = 2;
+
+
 class MenuState : public State {
 public:
 
-  sf::Text hello;
-  std::vector<MenuItem> menuitems;
   sf::Font font;
+  sf::Text hello;
+
+  const uint8_t MENU_LENGTH = 2;
+  std::vector<MenuItem> menuItems;  
   int selected = 0;
   
 public:
@@ -40,7 +48,23 @@ public:
 //MenuItem struct for each button
 struct MenuItem{
   std::string name;
+  sf::Text displayText;
   sf::RectangleShape rect;
   State* menState;
-    
+
+
+  inline void updateDisplayText() {
+    displayText.setString(name.c_str());
+  }
+  
+  MenuItem() {}
+  MenuItem(std::string name) {
+    this->name = name;
+    displayText.setString(name);
+  }
+  ~MenuItem() {
+    delete menState;
+    menState = nullptr;
+  }
+  
 };
